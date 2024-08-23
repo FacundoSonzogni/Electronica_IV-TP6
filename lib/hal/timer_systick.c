7b6e738e6dcc8e4471adf163e2d2fda5f2d2afbf
FacundoSonzogni <153464716+FacundoSonzogni@users.noreply.github.com>
2024-08-23 09:21:08 -0300

    Update timer_systick

--- lib/hal/timer_systick.c
+++ lib/hal/timer_systick.c
@@ -12,12 +12,12 @@ uint32_t Temporizador_obtMilisegundos(void){
     return tiempo;
 }
 
-void Temporizador_esperaMilis(uint32_t tiempo){
+void Temporizador_esperaMilis(uint32_t espera){
     const uint32_t inicial = tiempo;
-    while(tiempo-inicial < tiempo);
+    while(tiempo-inicial < espera);
 }
 
-void Temporizador_Handler(void){
+void SysTick_Handler(void){
     ++tiempo;
 }
 
